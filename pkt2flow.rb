f112fb7664dfd3396f39afafc5185c7e9b2bfd0d
Lars Eggert <lars@eggert.org>
2016-02-25 15:35:24 +0100

    Add head for pkt2flow

--- pkt2flow.rb
+++ pkt2flow.rb
@@ -3,6 +3,7 @@ class Pkt2flow < Formula
   homepage "https://github.com/caesar0301/pkt2flow"
   url "https://github.com/caesar0301/pkt2flow/archive/v1.2.tar.gz"
   sha256 "deb4c5dc2fd11c45e338e9d0c0127df03257040127c49016e40aaf491e5674a6"
+  head "https://github.com/caesar0301/pkt2flow.git"
 
   depends_on "scons" => :build
 
