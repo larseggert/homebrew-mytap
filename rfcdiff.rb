c3f5ca69629537fad3b9b99e3ab1f845bd757640
Lars Eggert <lars@eggert.org>
2024-12-10 09:33:40 +0200

    Latest version of rfcdiff

--- rfcdiff.rb
+++ rfcdiff.rb
@@ -1,17 +1,21 @@
 class Rfcdiff < Formula
   desc "Generate diffs between RFCs or Internet-Drafts"
-  homepage "https://tools.ietf.org/tools/rfcdiff/"
-  url "https://tools.ietf.org/tools/rfcdiff/rfcdiff-1.46.tgz"
-  sha256 "f4ab321e88b3d76c550545d9adc19b665435929bd8e1d0ed41d4f65eb2f77bcb"
+  homepage "https://github.com/ietf-tools/rfcdiff"
+  url "https://github.com/ietf-tools/rfcdiff/archive/refs/tags/1.48.tar.gz"
+  sha256 "11d08d059716275c3f2d25e03d1711be83bcf14de57a62fadf3406eaa0a75ade"
+  license "GPL-2.0-or-later"
 
   depends_on "gawk"
   depends_on "wdiff"
 
   def install
     bin.install "rfcdiff"
-    prefix.install "todo"
   end
 
   test do
+    (testpath/"a").write("a")
+    (testpath/"b").write("b")
+    system "bin/rfcdiff", "a", "b"
+    assert_predicate testpath/"b-from-a.diff.html", :exist?
   end
 end
